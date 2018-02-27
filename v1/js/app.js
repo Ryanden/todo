538a1a7d17fb0026d48905ee730b90b44ec9293f
hckang <hckang80@gmail.com>
2018-02-27 13:40:41 +0900

    Fixed to folder tree in v1

--- v1/js/app.js
+++ v1/js/app.js
@@ -49,31 +49,31 @@
   }
 
   window.addEventListener('load', function () {
-    // XMLHttpRequest 객체의 생성
-    var req = new XMLHttpRequest();
-    // 비동기 방식으로 Request를 오픈한다
-    req.open('GET', 'http://localhost:3000/todos');
-    // Request를 전송한다
-    req.send();
+    // // XMLHttpRequest 객체의 생성
+    // var req = new XMLHttpRequest();
+    // // 비동기 방식으로 Request를 오픈한다
+    // req.open('GET', 'http://localhost:3000/todos');
+    // // Request를 전송한다
+    // req.send();
 
-    // Event Handler
-    req.onreadystatechange = function () {
-      // 서버 응답 완료 && 정상 응답
-      if (req.readyState === XMLHttpRequest.DONE) {
-        if (req.status === 200) {
-          todos = JSON.parse(req.responseText);
-          rendarTodo();
-        } else {
-          console.log('[' + req.status + ']: ' + req.statusText);
-        }
-      }
-    };
-    // todos = [
-    //   { id: 3, content: 'HTML', completed: true },
-    //   { id: 2, content: 'CSS', completed: true },
-    //   { id: 1, content: 'Javascript', completed: false }
-    // ];
-    // rendarTodo();
+    // // Event Handler
+    // req.onreadystatechange = function () {
+    //   // 서버 응답 완료 && 정상 응답
+    //   if (req.readyState === XMLHttpRequest.DONE) {
+    //     if (req.status === 200) {
+    //       todos = JSON.parse(req.responseText);
+    //       rendarTodo();
+    //     } else {
+    //       console.log('[' + req.status + ']: ' + req.statusText);
+    //     }
+    //   }
+    // };
+    todos = [
+      { id: 3, content: 'HTML', completed: true },
+      { id: 2, content: 'CSS', completed: true },
+      { id: 1, content: 'Javascript', completed: false }
+    ];
+    rendarTodo();
   });
 
   inputTodo.addEventListener('keyup', function (e) {
